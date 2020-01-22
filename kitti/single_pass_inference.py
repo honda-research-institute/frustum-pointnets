c5a7c73b60e913dc7255c4d9b46125d2a2cc3995
Jiawei Huang <decompilation@gmail.com>
2020-01-22 12:26:40 -0800

    Finished visualization of 3D boxes on image from arbitrary KITTI sample.

--- kitti/single_pass_inference.py
+++ kitti/single_pass_inference.py
@@ -12,16 +12,18 @@ import tensorflow as tf
 import importlib
 import cv2
 from PIL import Image
+import matplotlib.pyplot as plt
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
+sys.path.append(os.path.join(ROOT_DIR, 'train'))
+from provider import from_prediction_to_label_format
 import kitti_util as utils
 import cPickle as pickle
 import argparse
 
-g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
 modelname = "frustum_pointnets_v1"
 MODEL = importlib.import_module(modelname)
 MODEL_PATH = "train/log_v1/model.ckpt"
@@ -32,6 +34,21 @@ NUM_CHANNEL = 4
 NUM_CLASSES = 2
 NUM_HEADING_BIN = 12
 NUM_SIZE_CLUSTER = 8  # one cluster for each type
+g_type2class = {'Car': 0, 'Van': 1, 'Truck': 2, 'Pedestrian': 3,
+                'Person_sitting': 4, 'Cyclist': 5, 'Tram': 6, 'Misc': 7}
+g_class2type = {g_type2class[t]: t for t in g_type2class}
+g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
+g_type_mean_size = {'Car': np.array([3.88311640418, 1.62856739989, 1.52563191462]),
+                    'Van': np.array([5.06763659, 1.9007158, 2.20532825]),
+                    'Truck': np.array([10.13586957, 2.58549199, 3.2520595]),
+                    'Pedestrian': np.array([0.84422524, 0.66068622, 1.76255119]),
+                    'Person_sitting': np.array([0.80057803, 0.5983815, 1.27450867]),
+                    'Cyclist': np.array([1.76282397, 0.59706367, 1.73698127]),
+                    'Tram': np.array([16.17150617, 2.53246914, 3.53079012]),
+                    'Misc': np.array([3.64300781, 1.54298177, 1.92320313])}
+g_mean_size_arr = np.zeros((NUM_SIZE_CLUSTER, 3))  # size clustrs
+for i in range(NUM_SIZE_CLUSTER):
+    g_mean_size_arr[i, :] = g_type_mean_size[g_class2type[i]]
 
 
 # scan is Nx4 array
@@ -165,7 +182,8 @@ def extract_frustum_data_rgb_detection(det_filename, image_filename,
                                        lidar_filename, calib_filename,
                                        type_whitelist=['Car'],
                                        write_to_pcd=False,
-                                       img_height_threshold=25,
+                                       draw_img=False,
+                                       img_height_threshold=20,
                                        lidar_point_threshold=5):
     det_id_list, det_type_list, det_box2d_list, det_prob_list = \
         read_det_file(det_filename)
@@ -178,6 +196,7 @@ def extract_frustum_data_rgb_detection(det_filename, image_filename,
     prob_list = []
     input_list = []  # channel number = 4, xyz,intensity in rect camera coord
     frustum_angle_list = []  # angle of 2d box center from pos x-axis
+    calib_list = []  # calib data
 
     # Foreach 2D BBox in detection file, crop corresponding frustum_pc
     for det_idx in range(len(det_id_list)):
@@ -193,6 +212,9 @@ def extract_frustum_data_rgb_detection(det_filename, image_filename,
             pc_rect[:, 0:3] = calib.project_velo_to_rect(pc_velo[:, 0:3])
             pc_rect[:, 3] = pc_velo[:, 3]
             img = get_kitti_image(image_filename)
+            if draw_img:
+                boxes2d = np.stack(det_box2d_list, axis=0)  # Nx4
+                draw_boxes2d_on_img(boxes2d, img)
             img_height, img_width, img_channel = img.shape
             _, pc_image_coord, img_fov_inds = get_lidar_in_image_fov( \
                 pc_velo[:, 0:3], calib, 0, 0, img_width, img_height, True)
@@ -232,6 +254,7 @@ def extract_frustum_data_rgb_detection(det_filename, image_filename,
         prob_list.append(det_prob_list[det_idx])
         input_list.append(pc_in_box_fov)
         frustum_angle_list.append(frustum_angle)
+        calib_list.append(calib)
 
     if False:
         with open(output_filename, 'wb') as fp:
@@ -241,6 +264,7 @@ def extract_frustum_data_rgb_detection(det_filename, image_filename,
             pickle.dump(type_list, fp)
             pickle.dump(frustum_angle_list, fp)
             pickle.dump(prob_list, fp)
+            pickle.dump(calib_list, fp)
 
     bsize = len(input_list)
     assert bsize <= BATCH_SIZE, 'num detections must be <= {}'.format(BATCH_SIZE)
@@ -259,7 +283,7 @@ def extract_frustum_data_rgb_detection(det_filename, image_filename,
         batch_prob[i] = prob
 
     # print("batch_rot_angle={}".format(batch_rot_angle))
-    return batch_data, batch_rot_angle, batch_prob, batch_one_hot_vec
+    return batch_data, batch_rot_angle, batch_prob, batch_one_hot_vec, calib, img
 
 
 def get_session_and_ops(batch_size, num_point):
@@ -310,6 +334,43 @@ def softmax(x):
     probs = np.exp(x - np.max(x, axis=len(shape) - 1, keepdims=True))
     probs /= np.sum(probs, axis=len(shape) - 1, keepdims=True)
     return probs
+
+
+# boxes2d = (n,4) => xmin, ymin, xmax, ymax
+def draw_boxes2d_on_img(boxes2d, img, render=True):
+    img1 = np.copy(img)  # for 2d bbox
+    for box2d in boxes2d:
+        cv2.rectangle(img1, (int(box2d[0]), int(box2d[1])),
+                      (int(box2d[2]), int(box2d[3])), (0, 255, 0), 2)
+    if render:
+        Image.fromarray(img1).show()
+
+
+# boxes3d = (n,8,2) => 8 corners per box with (x,y)
+def draw_boxes3d_on_img(boxes3d, img, color=(255, 0, 0), thickness=1, render=True):
+    # img1 = np.copy(img)  # for 3d bbox
+    boxes3d = boxes3d.astype(np.int32)
+    for box3d in boxes3d:  # box3d is (8,2)
+        for k in range(0, 4):
+            i, j = k, (k + 1) % 4
+            # use LINE_AA for opencv3
+            cv2.line(img, (box3d[i, 0], box3d[i, 1]), (box3d[j, 0], box3d[j, 1]), color, thickness, cv2.LINE_AA)
+
+            i, j = k + 4, (k + 1) % 4 + 4
+            cv2.line(img, (box3d[i, 0], box3d[i, 1]), (box3d[j, 0], box3d[j, 1]), color, thickness, cv2.LINE_AA)
+
+            i, j = k, k + 4
+            cv2.line(img, (box3d[i, 0], box3d[i, 1]), (box3d[j, 0], box3d[j, 1]), color, thickness, cv2.LINE_AA)
+    if render:
+        # Image.fromarray(img).show()
+        plt.imshow(img)
+        plt.show(block=False)
+        raw_input()
+        plt.clf()  # will make the plot window empty
+
+
+def render_img(img):
+    Image.fromarray(img).show()
 
 
 def inference(sess, ops, pc, one_hot_vec, batch_size):
@@ -368,7 +429,7 @@ def inference(sess, ops, pc, one_hot_vec, batch_size):
            size_cls, size_res, scores
 
 
-def run_inference(batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec):
+def run_inference(batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec, calib, img):
     batch_size = BATCH_SIZE
     batch_data_to_feed = np.zeros((batch_size, NUM_POINT, NUM_CHANNEL))
     batch_one_hot_to_feed = np.zeros((batch_size, 3))
@@ -384,26 +445,46 @@ def run_inference(batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec
         inference(sess, ops, batch_data_to_feed,
                   batch_one_hot_to_feed, batch_size=batch_size)
 
-    print("center=")
-    print(batch_center_pred[:cur_batch_size, :])
-    print("heading=")
-    print(batch_hclass_pred[:cur_batch_size])
+    # print("center=")
+    # print(batch_center_pred[:cur_batch_size, :])
+    # print("heading=")
+    # print(batch_hclass_pred[:cur_batch_size])
     # print("size=")
     # print(batch_sclass_pred[:cur_batch_size])
+
+    # Visualize network output on image
+    # boxes3d = (n,8,2)
+    boxes3d = np.zeros((cur_batch_size, 8, 2))
+    for pick in range(cur_batch_size):
+        print("pick={}", pick)
+        h, w, l, tx, ty, tz, ry = from_prediction_to_label_format(batch_center_pred[pick],
+                                                                  batch_hclass_pred[pick],
+                                                                  batch_hres_pred[pick],
+                                                                  batch_sclass_pred[pick],
+                                                                  batch_sres_pred[pick],
+                                                                  batch_rot_angle[pick])
+        print("center={:.1f}, {:.1f}, {:.1f}, l/w/h={:.1f}, {:.1f}, {:.1f}, ry={:.2f}".format(tx, ty, tz, l, w, h, ry))
+        # compute_projected_box3d returns (8,2)
+        boxes3d[pick] = utils.compute_projected_box3d(h, w, l, tx, ty, tz, ry, calib.P)
+
+    draw_boxes3d_on_img(boxes3d, img)
 
 
 if __name__ == '__main__':
     type_whitelist = ['Car']  # ['Car', 'Pedestrian', 'Cyclist']
     write_to_pcd = False
+    draw_img = False
 
     INPUT_DIR = os.path.join(ROOT_DIR, 'jhuang')
-    SAMPLE = '001960'  # '000851' '000006', '000003'
-    batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec = \
-        extract_frustum_data_rgb_detection(os.path.join(INPUT_DIR, "detections_{:s}.txt".format(SAMPLE)),
-                                           os.path.join(INPUT_DIR, "{:s}.png".format(SAMPLE)),
-                                           os.path.join(INPUT_DIR, "{:s}.bin".format(SAMPLE)),
-                                           os.path.join(INPUT_DIR, "{:s}.txt".format(SAMPLE)),
-                                           type_whitelist=type_whitelist,
-                                           write_to_pcd=write_to_pcd)
-
-    run_inference(batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec)
+    SAMPLES = ['001960', '000851', '000006', '000003']
+    for SAMPLE in SAMPLES:
+        batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec, calib, img = \
+            extract_frustum_data_rgb_detection(os.path.join(INPUT_DIR, "detections_{:s}.txt".format(SAMPLE)),
+                                               os.path.join(INPUT_DIR, "{:s}.png".format(SAMPLE)),
+                                               os.path.join(INPUT_DIR, "{:s}.bin".format(SAMPLE)),
+                                               os.path.join(INPUT_DIR, "{:s}.txt".format(SAMPLE)),
+                                               type_whitelist=type_whitelist,
+                                               write_to_pcd=write_to_pcd,
+                                               draw_img=draw_img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        run_inference(batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec, calib, img)
