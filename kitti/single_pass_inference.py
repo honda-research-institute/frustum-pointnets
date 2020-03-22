11422e7880529a034c2d5b7598e7e1ac563f187d
Jiawei Huang <decompilation@gmail.com>
2020-03-21 21:40:09 -0700

    Adding more testing samples.

--- kitti/single_pass_inference.py
+++ kitti/single_pass_inference.py
@@ -528,7 +528,7 @@ def run_inference(sess, ops, batch_data, batch_rot_angle, batch_rgb_prob, batch_
 
 if __name__ == '__main__':
     write_frustum_pcd = False
-    draw_img = False
+    draw_img = True
 
     run_mode = 2  # 0: kitti, 1: mule VLP32, 2: nuscenes
     if run_mode == 0:  # kitti
@@ -547,11 +547,10 @@ if __name__ == '__main__':
         g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Truck': 2}
         INPUT_DIR = os.path.join(ROOT_DIR, 'jhuang', 'nuscenes')
         lidar_format = 'nusc'
-        sel = 14
-        if sel == 14:
-            DET_FILE = os.path.join(INPUT_DIR, "00014.txt")
-            IMG_FILE = os.path.join(INPUT_DIR, "00014.png")
-            LIDAR_FILE = os.path.join(INPUT_DIR, "00014.bin")
+        sel = 27 #6, 13, 14, 18, 23, 27, 34, 39
+        DET_FILE = os.path.join(INPUT_DIR, "{:05d}.txt".format(sel))
+        IMG_FILE = os.path.join(INPUT_DIR, "{:05d}.png".format(sel))
+        LIDAR_FILE = os.path.join(INPUT_DIR, "{:05d}.bin".format(sel))
         CALIB_FILE = os.path.join(INPUT_DIR, "calib.txt")
         SAMPLES = ['0']  # dummy
 
