b3fc8fbe48fd976169391f78d0c2ed9b8262cd11
Jiawei Huang <decompilation@gmail.com>
2020-03-22 14:04:38 -0700

    Unified mule and nusc testing.

--- kitti/single_pass_inference.py
+++ kitti/single_pass_inference.py
@@ -537,37 +537,40 @@ if __name__ == '__main__':
     run_mode = 2  # 0: kitti, 1: mule VLP32, 2: nuscenes
     if run_mode == 0:  # kitti
         g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
-        lidar_ending = ".bin"
         INPUT_DIR = os.path.join(ROOT_DIR, 'jhuang', 'kitti')
         SAMPLES = ['001960', '000851', '000006', '000003']
         lidar_format = 'kitti'
     elif run_mode == 1:  # mule
         g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Cyclist': 2}
-        lidar_ending = ".pcd"
         INPUT_DIR = os.path.join(ROOT_DIR, 'jhuang', 'VLP32')
-        SAMPLES = ['000040']  # ['000100'] #['000010']
+        CALIB_FILE = os.path.join(INPUT_DIR, "calib.txt")
+        SAMPLES = ['000610', '000267', '000072', '000220', '000100', '000010', '000040']
         lidar_format = 'mule'
     else:  # nuscenes
         g_type2onehotclass = {'Car': 0, 'Pedestrian': 1, 'Truck': 2}
         INPUT_DIR = os.path.join(ROOT_DIR, 'jhuang', 'nuscenes')
         lidar_format = 'nusc'
-        sel = 34 #6, 13, 14, 18, 23, 27, 34, 39
-        DET_FILE = os.path.join(INPUT_DIR, "{:05d}.txt".format(sel))
-        IMG_FILE = os.path.join(INPUT_DIR, "{:05d}.png".format(sel))
-        LIDAR_FILE = os.path.join(INPUT_DIR, "{:05d}.bin".format(sel))
         CALIB_FILE = os.path.join(INPUT_DIR, "calib.txt")
-        SAMPLES = ['0']  # dummy
+        SAMPLES = [6, 13, 14, 18, 23, 27, 34, 39]
 
     type_whitelist = g_type2onehotclass.keys() # ['Car', 'Pedestrian', 'Cyclist', ...]
     batch_size = BATCH_SIZE
     sess, ops = get_session_and_ops(batch_size=batch_size, num_point=NUM_POINT)
 
     for SAMPLE in SAMPLES:
-        if run_mode == 0 or run_mode == 1:
+        if run_mode == 0:
             DET_FILE = os.path.join(INPUT_DIR, "detections_{:s}.txt".format(SAMPLE))
             IMG_FILE = os.path.join(INPUT_DIR, "{:s}.png".format(SAMPLE))
-            LIDAR_FILE = os.path.join(INPUT_DIR, "{:s}{:s}".format(SAMPLE, lidar_ending))
+            LIDAR_FILE = os.path.join(INPUT_DIR, "{:s}.bin".format(SAMPLE))
             CALIB_FILE = os.path.join(INPUT_DIR, "{:s}.txt".format(SAMPLE))
+        elif run_mode == 1:
+            DET_FILE = os.path.join(INPUT_DIR, "{:s}.txt".format(SAMPLE))
+            IMG_FILE = os.path.join(INPUT_DIR, "{:s}.png".format(SAMPLE))
+            LIDAR_FILE = os.path.join(INPUT_DIR, "{:s}.pcd".format(SAMPLE))
+        else:
+            DET_FILE = os.path.join(INPUT_DIR, "{:05d}.txt".format(SAMPLE))
+            IMG_FILE = os.path.join(INPUT_DIR, "{:05d}.png".format(SAMPLE))
+            LIDAR_FILE = os.path.join(INPUT_DIR, "{:05d}.bin".format(SAMPLE))
 
         t0 = time.time()  # start time
         batch_data, batch_rot_angle, batch_rgb_prob, batch_one_hot_vec, calib, img = \
